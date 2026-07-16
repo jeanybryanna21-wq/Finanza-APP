609297ea4c57514f299d5fe69d4affba94646c46
jeanybryanna21-wq <jeanybryanna21@gmail.com>
2026-07-16 08:26:05 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='instara-ai-rc138-disable-direct-snapshot';
+const CACHE='instara-ai-rc139-force-bypasses-304';
 const CORE=[
   './','./index.html','./bitacora-de-trabajo.html','./fondo-app.webp','./manifest.webmanifest','./sw.js',
   './assets/leaflet/leaflet.css','./assets/leaflet/leaflet.js','./assets/maplibre/maplibre-gl.css','./assets/maplibre/maplibre-gl.js','./assets/sortable.min.js',
